16f323fed52002d3b0e2be38d4ebb34badb35ca8
HenriqueCesarS <rm94276@fiap.com.br>
2022-06-21 17:54:39 -0300

    Versão01

---
+++ JS/scriptAcesso.js
@@ -0,0 +1,13 @@
+const btnConfirmar = document.getElementById('confirmId')
+
+btnConfirmar.addEventListener('click', ()=>{
+    const campos = document.querySelectorAll('.inserir') 
+    for(let i= 0; i < campos.length; i++){
+        if(campos[i].value == ""){
+            alert("O campo "+ campos[i].placeholder + " não foi preenchido")
+                return
+        }
+    }
+    document.querySelector('.cadastro').submit()
+        alert("Dados cadastrados!")
+})
